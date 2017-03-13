f8e495e42be0d6eebd212d9d5e6594e8d12e0615
Brandon Keepers <bkeepers@github.com>
2017-03-13 07:34:32 -0500

    Run against all integrations

--- index.js
+++ index.js
@@ -2,27 +2,39 @@ const Stale = require('./lib/stale');
 
 // TODO:
 // - [ ] On an interval:
-//   - [ ] Get all installations & repositories
-//   - [ ] Run mark & sweep
+//   - [x] Get all installations & repositories
+//   - [x] Run mark & sweep
 // - [ ] on relevant issue activity: unmark
 // - [ ] Get config from repo or org
 
-// Hackery to test against a single repository
-const GitHubApi = require("github");
-const github = new GitHubApi({
-    debug: true
-});
+const INTERVAL = 30 * 1000; // check every hour
 
-// user token
-github.authenticate({
-    type: "token",
-    token: process.env.GITHUB_TOKEN,
-});
+module.exports = async function(robot) {
+  setInterval(check, INTERVAL);
+  check(); // check on startup
 
-const stale = new Stale(github, {owner: "robotland", repo: "test", perform: false});
-stale.markAndSweep();
+  async function check() {
+    robot.log.info("Checking for stale issues");
 
-// Show trace for any unhandled rejections
-process.on('unhandledRejection', reason => {
-  console.error(reason);
-});
+    const github = await robot.integration.asIntegration();
+    // TODO: Pagination
+    const installations = await github.integrations.getInstallations({});
+    installations.forEach(async installation => {
+      const client = await robot.auth(installation.id);
+      // TODO: Pagination
+      const data = await client.integrations.getInstallationRepositories({});
+      return data.repositories.map(repo => checkRepository(client, repo));
+    });
+  }
+
+  async function checkRepository(client, repository) {
+    const stale = new Stale(client, {
+      owner: repository.owner.login,
+      repo: repository.name,
+      perform: false,
+      ttl: 1000 * 60 * 60 * 2,
+      logger: robot.log
+    });
+    return stale.markAndSweep();
+  }
+}
