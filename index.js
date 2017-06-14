40a5f13602510887fda443cb8ef48350644eb5d2
Lee Dohm <lee-dohm@users.noreply.github.com>
2017-06-14 13:04:49 -0700

    Add no-response behavior

--- index.js
+++ index.js
@@ -1,8 +1,62 @@
 const yaml = require('js-yaml');
 const visitor = require('probot-visitor');
+const NoResponse = require('./lib/no-response');
 const Stale = require('./lib/stale');
 
-module.exports = async robot => {
+async function noResponseBehavior(robot) {
+  // Visit all repositories to sweep issues with no response
+  const visit = visitor(robot, sweep);
+
+  // Remove response required label if the author comments
+  robot.on('issue_comment', unmark);
+
+  async function sweep(installation, repository) {
+    const github = await robot.auth(installation.id);
+    const noResponse = await forRepository(github, repository);
+    if (noResponse.config.exists) {
+      return noResponse.sweep();
+    }
+  }
+
+  async function unmark(event, context) {
+    if (!context.isBot) {
+      const github = await robot.auth(event.payload.installation.id);
+      const noResponse = await forRepository(github, event.payload.repository);
+      const issue = event.payload.issue;
+      const comment = event.payload.comment;
+
+      if (noResponse.config.exists) {
+        if (noResponse.hasResponseRequiredLabel(issue) && issue.user.login === comment.user.login) {
+          noResponse.unmark(issue);
+        }
+      }
+    }
+  }
+
+  async function forRepository(github, repository) {
+    const owner = repository.owner.login;
+    const repo = repository.name;
+    const path = '.github/stale.yml';
+    let config;
+
+    try {
+      const data = await github.repos.getContent({owner, repo, path});
+      config = yaml.load(new Buffer(data.content, 'base64').toString()).noResponse || {};
+      config.exists = true;
+    } catch (err) {
+      robot.log.debug(err, 'No configuration file found');
+      visit.stop(repository);
+      // Don't perform for repository without a config
+      config = {exists: false};
+    }
+
+    config = Object.assign(config, {owner, repo, logger: robot.log});
+
+    return new NoResponse(github, config);
+  }
+}
+
+async function staleBehavior(robot) {
   // Visit all repositories to mark and sweep stale issues
   const visit = visitor(robot, markAndSweep);
 
@@ -49,6 +103,10 @@ module.exports = async robot => {
     try {
       const res = await github.repos.getContent({owner, repo, path});
       config = yaml.load(new Buffer(res.data.content, 'base64').toString()) || {};
+
+      if (config.stale) {
+        config = config.stale;
+      }
     } catch (err) {
       visit.stop(repository);
       // Don't actually perform for repository without a config
@@ -59,4 +117,9 @@ module.exports = async robot => {
 
     return new Stale(github, config);
   }
+}
+
+module.exports = async robot => {
+  noResponseBehavior(robot);
+  staleBehavior(robot);
 };
