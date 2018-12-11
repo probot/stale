68cc7e55a436cb67f5c779da4c1bcdbb54109f58
Waldir Pimenta <waldyrious@gmail.com>
2018-12-11 08:25:09 +0000

    Update tests to match new default comment

--- test/schema.test.js
+++ test/schema.test.js
@@ -60,9 +60,8 @@ describe('schema', () => {
       exemptMilestones: false,
       staleLabel: 'wontfix',
       perform: true,
-      markComment: 'This issue has been automatically marked as stale because ' +
-        'it has not had recent activity. It will be closed if no further ' +
-        'activity occurs. Thank you for your contributions.',
+      markComment: 'Is this still relevant? If so, what is blocking it? ' +
+        'Is there anything you can do to help move it forward?',
       unmarkComment: false,
       closeComment: false,
       limitPerRun: 30
